e38af2369eba44468e1578dc6ca14232fc18e062
mohammedbarhoum <115804765+mohammedbarhoum@users.noreply.github.com>
2022-12-08 16:17:55 +0200

    smartbut test

--- Autong.java
+++ Autong.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,11 +16,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Autong {
  
-	public WebDriver driver1;
+	public WebDriver driver;
 	
-	public WebDriver driver2;
-	
-	public WebDriver driver3;
 	
 	
 	@BeforeTest
@@ -25,39 +26,33 @@ public class Autong {
 		
 		WebDriverManager.edgedriver().setup();
 	
-		driver1= new EdgeDriver();
+		driver= new EdgeDriver();
 		
 		
 		
 		
 		
-		driver1.get("https://github.com/login");
+		driver.get("https://github.com/login");
 		
-		driver1.findElement(By.xpath("//*[@id=\"login_field\"]")).sendKeys("mohammedbarhoum777@gmial.com");
-		driver1.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("mohammed12345$");
-		driver1.findElement(By.xpath("//*[@id=\"login\"]/div[4]/form/div/input[11]")).click();
+		driver.findElement(By.xpath("//*[@id=\"login_field\"]")).sendKeys("mohammedbarhoum777@gmial.com");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("mohammed12345$");
+		driver.findElement(By.xpath("//*[@id=\"login\"]/div[4]/form/div/input[11]")).click();
 		
 	}
 	@Test()
 	
 	public void Test_Title()
 	{
-		String ActualTitleName =driver1.getTitle();
+		String ActualTitleName =driver.getTitle();
 		 String ExpectedTittle="Gethub";
 		
 		Assert.assertEquals("ActualTitleName", "ExpectedTittle");
 		
 	}
-	@Test()
-	
-	public void Test_Login()
-	{
-		System.out.println("");
-		
-		
 	
 	}
-	}
+
+	
 	
 	
 	
